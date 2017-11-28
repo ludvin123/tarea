414e9e41cafe542c7675e88984d9259c208bf290
Alumno05 J601 <a05j601@campustls.local>
2017-11-28 12:27:30 -0500

    agregar tarea

--- public/vainillajs-eventos/main.js
+++ public/vainillajs-eventos/main.js
@@ -1,11 +1,45 @@
 document.addEventListener("DOMContentLoaded" , function(){
   console.log("Estoy listo!!");
+/*
+  var forms = document.querySelectorAll("form");
+  var form =forms[0];  // Primera coincidencia
 
-  var form = document.querySelectorAll("form");
-
-  form[0].addEventListener('submit' , function(){
+  form.addEventListener('submit' , function(){
     event.preventDefault();
-    console.log("Enviado")
+    var first_name = document.querySelectorAll("#first_name")[0].value;
+
+    var message = document.querySelectorAll("#message")[0];
+    message.innerHTML = message.innerHTML + "<span>" + first_name + "</span>";
+    //console.log(first_name);
+
   });
   console.log(form);
+})
+*/
+/*
+var elements = document.querySelectorAll("li");
+
+for(var i = 0;i < elements.length; i=i + 1) {
+  var element = elements[i];
+
+  element.addEventListener("click", function(event)) {
+    var el = event.target;
+    console.log(el.innerHTML);
+  });
+}
+*/
+var lista = document.querySelectorAll("ul")[0];
+lista.addEventListener("click", function(event){
+  var el = event.target;
+
+  console.log(el)
+
+  /*if (el.tagName.toLowerCase() === "li") {
+    console.log("Click en:" , el.innerHTML);
+  }
+  */
+  if (el.classList.contains("item")) {
+    console.log("Click en:" , el.innerHTML);
+  }
+});
 })
